2049bbbb8bb2bb2bbf9a534fe8c24df9f83068fa
Marco Cedaro <cedmax@users.noreply.github.com>
2019-06-28 13:18:22 +0100

    removed the need for preloading

--- src/components/FalseMaster.js
+++ src/components/FalseMaster.js
@@ -1,16 +1,17 @@
 import React from "react";
 
-export default ({ currentBackground, logoVisible, backgrounds }) => (
-  <div
-    className="false-master"
-    style={{
-      backgroundImage: `url(${`/images/${
-        backgrounds[currentBackground]
-      }.jpg`})`,
-    }}
-  >
-    <h1 style={{ opacity: logoVisible ? 0.9 : 0 }}>
-      <span>the</span>Invisible Cities<span>project</span>
-    </h1>
-  </div>
-);
+export default ({ currentBackground, logoVisible, backgrounds }) =>
+  backgrounds.map((background, i) => (
+    <div
+      key={background}
+      className="false-master"
+      style={{
+        opacity: currentBackground === i ? 1 : 0,
+        backgroundImage: `url(${`/images/${background}.jpg`})`,
+      }}
+    >
+      <h1 style={{ opacity: logoVisible ? 0.9 : 0 }}>
+        <span>the</span>Invisible Cities<span>project</span>
+      </h1>
+    </div>
+  ));
