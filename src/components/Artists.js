d687cfe15adfa40ad6be40dfef6db99acc3faab4
Marco Cedaro <cedmax@users.noreply.github.com>
2019-06-28 13:18:01 +0100

    better artist component

--- src/components/Artists.js
+++ src/components/Artists.js
@@ -2,11 +2,23 @@ import React, { memo, useState, useCallback, useEffect } from "react";
 import Modal from "react-modal";
 import BlockContent from "@sanity/block-content-to-react";
 
-const Artist = memo(({ image, name, role, content, api }) => {
+const Artist = memo(({ artist, show }) => {
+  return (
+    <div className="artist">
+      <button type="button" onClick={show(artist)}>
+        <img src={artist.image} alt={artist.name} />
+      </button>
+    </div>
+  );
+});
+
+export default memo(({ siteData: { artist: artists }, api }) => {
   const [visible, setVisible] = useState(false);
-  const show = useCallback(() => {
-    setVisible(true);
-  }, [setVisible]);
+  const show = useCallback(user =>
+    useCallback(() => {
+      setVisible(user);
+    }, [setVisible])
+  );
   const hide = useCallback(() => {
     setVisible(false);
   }, [setVisible]);
@@ -18,25 +30,21 @@ const Artist = memo(({ image, name, role, content, api }) => {
   }, [visible]);
 
   return (
-    <div className="artist">
-      <button type="button" onClick={show}>
-        <img src={image} alt={name} />
-      </button>
+    <div className="padder">
+      <div className="artists">
+        {artists.map(artist => (
+          <Artist show={show} key={artist.name} artist={artist} />
+        ))}
+      </div>
       <Modal onRequestClose={hide} isOpen={visible}>
-        <h3>{name}</h3>
-        <h5>{role}</h5>
-        <BlockContent blocks={content} />
+        {visible && (
+          <>
+            <h3>{visible.name}</h3>
+            <h5>{visible.role}</h5>
+            <BlockContent blocks={visible.content} />
+          </>
+        )}
       </Modal>
     </div>
   );
 });
-
-export default memo(({ siteData: { artist: artists }, api }) => (
-  <div className="padder">
-    <div className="artists">
-      {artists.map(artist => (
-        <Artist key={artist.name} api={api} {...artist} />
-      ))}
-    </div>
-  </div>
-));
