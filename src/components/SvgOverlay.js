fc2f08f7990e538be9c43b13fb8e001521eb5da6
Marco Cedaro <cedmax@users.noreply.github.com>
2019-06-30 21:21:04 +0100

    fixed animation

--- src/components/SvgOverlay.js
+++ src/components/SvgOverlay.js
@@ -3,7 +3,7 @@ import React from "react";
 export default ({ name, isVisible, children }) => (
   <div
     style={{
-      "--background-position": isVisible ? "center 0" : "center 200vh",
+      "--background-position": isVisible ? "center bottom" : "center 200vh",
       "--background": `url('/images/${name}.svg')`,
     }}
     className="section component"
