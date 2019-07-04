1a57a45aadf88ca24891d2bd405753b4f94c7cc6
Marco Cedaro <cedmax@users.noreply.github.com>
2019-07-04 13:21:16 +0100

    added a section colour

--- src/components/Overlay.js
+++ src/components/Overlay.js
@@ -1,9 +1,10 @@
 import React from "react";
 
 const backgrounds = {
-  athens: "rgba(224, 17, 95, .7)",
+  athens: "rgb(0, 150, 152, .7)",
   wroclaw: "rgba(0, 128, 0, .7)",
   venice: "rgba(232, 41, 155, .7)",
+  edinburgh: "rgba(253, 190, 2, .7)",
 };
 
 export default ({ name, children, title }) => (
