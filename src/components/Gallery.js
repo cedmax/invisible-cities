39003d13c0273887fd6c239cde17f458f06bf728
Marco Cedaro <cedmax@users.noreply.github.com>
2019-07-26 21:39:56 +0100

    added flickr gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,3 +1,8 @@
 import React from 'react';
+import FlickrLightbox from 'react-flickr-lightbox'
 
-export default () => null
+export default () => (
+  <div className="padder gallery">
+    <FlickrLightbox className="gallery" api_key='3a19f674bf359f785ae9de1999183a3e' user_id='182945327@N07' />
+  </div>
+)
